37757bb30fbb3ebc4d07a2549d39b1e06eeaf7ea
DucNM <minhduc.ngo0911@gmail.com>
2020-05-16 20:59:09 +0700

    add release.config

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/ducnmhe130666/demo-react-app",
+    plugin: [
+        '@semantic-release/commit-analyzer', 
+        '@semantic-release/release-notes-generator', 
+        '@semantic-release/github'
+        ]
+
+}
